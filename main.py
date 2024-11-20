5310bb83d34c54bb2bf584fe8bae5563aa865915
jachinema <99571020+jachinema@users.noreply.github.com>
2024-11-20 04:23:25 -0600

    Design overhaul
    Revamped script ground up. Now much cleaner code along with following additions: zoom-in, zoom-out, massively improved logic, infinite grid, camera movement, replay button.

--- main.py
+++ main.py
@@ -1,134 +1,164 @@
-import os
-import random
-
-dependencies = ['pygame']
-
-try:
-    import pygame as pg
-except ImportError:
-    for dependency in dependencies:
-        os.system('pip install ' + dependency)
-    import pygame as pg
-
-pg.init()
-
-WINDOW = pg.display.set_mode((1920, 1080))
-WINDOW.fill((61, 61, 61))
-pg.display.update()
-###############################
+import pygame as pg
+from constants import CELL_COLOR, BACKGROUND, FRAMERATE, INITIAL_GRID_SIZE, RESOLUTION, SENSITIVITY
 
 
-class Cell:
-    grid = {}
+class Grid:
+    def __init__(self, points: set):
+        self.alive = points
 
-    def __init__(self, x, y, status=False):
+    def kill(self, point: tuple):
+        self.alive.remove(point)
 
-        self.x = x
-        self.y = y
-        self.status = status
+    def resurrect(self, point: tuple):
+        self.alive.add(point)
 
-        Cell.grid[(x, y)] = {
-            'obj': self,
-            'adj': dict()
-        }
+    def update(self):
+        adjacency = dict()
 
-    def get(self):
-        return self.status
+        for point in self.alive:
+            x, y = point
+            neighbors = [
+                (x+1, y),
+                (x-1, y),
+                (x, y+1),
+                (x, y-1),
+                (x+1, y+1),
+                (x-1, y-1),
+                (x+1, y-1),
+                (x-1, y+1)
+            ]
+            for neighbor in neighbors:
+                if neighbor not in adjacency:
+                    adjacency[neighbor] = 1
+                else:
+                    adjacency[neighbor] += 1
 
-    @classmethod
-    def set_grid(cls):
-        locales = [
-            (-1, -1),
-            (-1, 1),
-            (1, -1),
-            (1, 1),
-            (1, 0),
-            (0, 1),
-            (-1, 0),
-            (0, -1)
-        ]
+        prep_to_kill = set()
+        for point in self.alive:
+            if point not in adjacency or adjacency[point] not in (2, 3):
+                prep_to_kill.add(point)
 
-        for lc in cls.grid:
+        prep_to_ressurect = set()
+        for point in adjacency:
+            if point not in self.alive and adjacency[point] == 3:
+                prep_to_ressurect.add(point)
 
-            for dx, dy in locales:
-                cell = cls.grid[lc]
-                try:
-                    cls.grid[
-                        (cell['obj'].x + dx, cell['obj'].y + dy)
-                    ]['adj'][
-                        (cell['obj'].x, cell['obj'].y)
-                    ] = cell['obj'].get
-                except KeyError:
-                    pass # usually means corner has been hit
+        for point in prep_to_ressurect:
+            self.resurrect(point)
 
-    def draw(self):
-        color = self.status and (255, 255, 255)
-        color = color or (61, 61, 61)
-        pg.draw.rect(WINDOW, color, (self.x*20, self.y*20, 20, 20))
-
-    def __repr__(self):
-        return f'Cell({self.x}, {self.y}, status={self.status})'
-
-################################
+        return prep_to_kill
 
 
-for i in range(96):
-    for j in range(54):
-        Cell(i, j).draw()
+class Render:
+    def __init__(self, screen: pg.Surface, top=0, left=0):
+        self.camera_pos = top, left
+        self.camera_x = left
+        self.camera_y = top
+        self.screen = screen
+        self.color = CELL_COLOR
+        self.grid_size = INITIAL_GRID_SIZE
 
-pg.display.update()
-running = True
+    def render(self, grid: Grid, kill: set):
+        for point in grid.alive:
+            abs_x, abs_y = point
+            rel_x, rel_y = (abs_x - self.camera_x, abs_y - self.camera_y)
+            pg.draw.rect(self.screen, self.color,
+                         (rel_x*self.grid_size, rel_y*self.grid_size, self.grid_size, self.grid_size))
 
-Cell.set_grid()
-clicked = False
+        for point in kill:
+            self._remove(grid, point)
 
-if __name__ == "__main__":
-    while running:
+    def move(self, rel_x, rel_y):
+        self.camera_x += rel_x*SENSITIVITY
+        self.camera_y += rel_y*SENSITIVITY
+
+    def set_pos(self, x, y):
+        self.camera_x = x
+        self.camera_y = y
+
+    def _remove(self, grid: Grid, point: tuple):
+        abs_x, abs_y = point
+        rel_x, rel_y = (abs_x - self.camera_x, abs_y - self.camera_y)
+
+        grid.kill(point)
+        pg.draw.rect(self.screen, BACKGROUND,
+                     (rel_x*self.grid_size, rel_y*self.grid_size, self.grid_size, self.grid_size))
+
+    def zoom(self, delta):
+        self.grid_size += delta
+        self.screen.fill(BACKGROUND)
+
+
+def main(screen: pg.Surface, camera: Render):
+    screen.fill(BACKGROUND)
+    clock = pg.time.Clock()
+
+    pg.display.update()
+    points = set()
+    dragging = False
+
+    while True:
         for event in pg.event.get():
             if event.type == pg.QUIT:
-                running = False
-                break
-
-            if event.type == pg.MOUSEBUTTONDOWN and event.button == 1:
-                clicked = True
+                exit()
+            elif event.type == pg.MOUSEBUTTONDOWN and event.button == 1:
+                dragging = True
             elif event.type == pg.MOUSEBUTTONUP and event.button == 1:
-                clicked = False
+                dragging = False
 
-        if clicked:
-            x, y = pg.mouse.get_pos()
-            lx, ly = x-x%20, y-y%20
-            pg.draw.rect(WINDOW, (255, 255, 255), (lx, ly, 20, 20))
-            pg.display.update()
-            Cell.grid[(lx//20, ly//20)]['obj'].status = True
-        elif pg.key.get_pressed()[pg.K_SPACE]:
+        keys_pressed = pg.key.get_pressed()
+        if keys_pressed[pg.K_SPACE]:
             break
 
-    while running:
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
-                running = False
-                break
-
-        kill = []
-        revive = []
-        for cell in Cell.grid:
-            alive = 0
-            for adj in Cell.grid[cell]['adj']:
-                alive = alive + Cell.grid[cell]['adj'][adj]()
-
-            if not Cell.grid[cell]['obj'].get() and alive == 3:
-                revive.append(cell)
-            elif alive not in (2, 3):
-                kill.append(cell)
-
-        for k in kill:
-            Cell.grid[k]['obj'].status = False
-            pg.draw.rect(WINDOW, (61, 61, 61), (k[0]*20, k[1]*20, 20, 20))
-
-        for r in revive:
-            Cell.grid[r]['obj'].status = True
-            pg.draw.rect(WINDOW, (255, 255, 255), (r[0]*20, r[1]*20, 20, 20))
+        if dragging:
+            x, y = pg.mouse.get_pos()
+            row, col = x//INITIAL_GRID_SIZE, y//INITIAL_GRID_SIZE
+            points.add((row, col))
+            pg.draw.rect(camera.screen, camera.color,
+                         (row * camera.grid_size, col * camera.grid_size, camera.grid_size, camera.grid_size))
 
         pg.display.update()
-        kill = []
-        revive = []
+
+    grid = Grid(points)
+    camera.render(grid, set())
+    dragging = False
+
+    while True:
+        clock.tick(FRAMERATE)
+
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                exit()
+            elif dragging and event.type == pg.MOUSEMOTION:
+                rel_x, rel_y = event.rel
+                camera.move(rel_x, rel_y)
+                screen.fill(BACKGROUND)
+
+            elif event.type == pg.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    dragging = True
+                elif event.button == 4:
+                    camera.zoom(1)
+                elif event.button == 5:
+                    camera.zoom(-1)
+            elif event.type == pg.MOUSEBUTTONUP:
+                if event.button == 1:
+                    dragging = False
+
+        kill = grid.update()
+        camera.render(grid, kill)
+
+        pg.display.update()
+
+        keys_pressed = pg.key.get_pressed()
+        if keys_pressed[pg.K_r]:
+            camera.set_pos(0, 0)
+            camera.grid_size = INITIAL_GRID_SIZE
+            main(screen, camera)
+
+
+if __name__ == '__main__':
+    pg.init()
+    scr = pg.display.set_mode(RESOLUTION)
+    main(scr, Render(scr))
+
